210c9cb4d2a8f4d645501d627c60bb16a1752734
BugHunter-X <askzenai@gmail.com>
2024-12-07 14:10:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+```javascript
+<div class="flex overflow-x-auto">
+  <div class="bg-red-500 p-4 w-1/2 flex-shrink-0"></div>
+  <div class="bg-blue-500 p-4 flex-shrink-0">
+    <p>This is a long paragraph of text that might cause overflow.</p>
+    <p>More text here to ensure overflow.</p>
+  </div>
+</div>
+```
