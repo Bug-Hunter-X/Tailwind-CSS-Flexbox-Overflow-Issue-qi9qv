052675767d26f68ca98a0cddddd61322ab3839dc
BugHunter-X <askzenai@gmail.com>
2024-12-07 14:10:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="flex">
+  <div class="bg-red-500 p-4 w-1/2"></div>
+  <div class="bg-blue-500 p-4"></div>
+</div>
+```This code snippet uses Tailwind CSS classes to create a flex container with two divs. The first div has a red background and takes up half the width, while the second div has a blue background and takes up the remaining half. However, if you add content to the second div that exceeds the available width, it will overflow and break the layout.
